eba1a2bcdb2eb5d9d76d670aa4a2ef7a4bc43857
SAKSHI RAJ <SAKSHI RAJ@192.168.0.4>
2023-11-10 21:57:12 +0530

    Name unicode

---
+++ src/selenium_course1/Name.java
@@ -0,0 +1,19 @@
+package selenium_course1;
+
+public class Name {
+	public static void main(String[] args) {
+	char c1 = '\u0938';
+
+char c2 = '\u0964';
+char c3 ='\u0960';
+char c4='\u0940';
+char c5='\u0930';
+char c6='\u0964';
+char c7='\u091C';
+
+
+System.out.println(c1+""+c2+""+c3+""+c4+" "+c5+""+c6+""+c7+"");
+	}
+
+
+}
