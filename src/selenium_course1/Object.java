bf6f042de635ab5d3ca42ce45e928359e279376e
SAKSHI RAJ <SAKSHI RAJ@192.168.0.5>
2023-11-30 09:55:40 +0530

    Objects

---
+++ src/selenium_course1/Object.java
@@ -0,0 +1,215 @@
+package selenium_course1;
+
+public class Object {
+
+	public static void main(String[] args) {
+		
+		class Human{
+			//characteristics:- name,age,height,weight
+			// Behaviour:- to speak()
+			
+			String name;
+			int age;
+			double height;
+			double weight;
+			String country;
+			
+			void speak(String content) {
+			}
+		}
+			
+			class Bag{
+				//characteristics:- brand,size,weight
+				// Behaviour:- carry()
+				
+				String brand;
+				int zip;
+				double size;
+				double weight;
+				
+				
+				void carry(String content) {
+			
+			}
+			
+		}
+			class Bed{
+				//characteristics:- length,weight, shape,
+				// Behaviour:- tosleep()
+				
+				String shape;
+				double length;
+				double weight;
+				
+				
+				void tosleep(String content) {
+			
+	}
+}
+			class Fan{
+				//characteristics:- shape,bladeCount
+				// Behaviour:- air()
+				
+				String shape;
+				int bladeCount;
+				
+				
+				void air(String content) {
+			
+	}
+				}
+			class notebook{
+				//characteristics:- shape,height,page
+				// Behaviour:- towrite()
+				
+				String shape;
+				double height;
+				int page;				
+				void towrite(String content) {
+					
+					
+				}
+			
+			
+	}
+			class table{
+				//characteristics:- length,shape,tableleg
+				// Behaviour:- tokeep()
+				
+				String shape;
+				double length;
+				int tableleg;
+				
+				
+				void tokeep(String content) {
+			
+	}
+}
+			class laptop{
+				//characteristics:- length,size,brand,processor
+				// Behaviour:- toWork()
+				
+				String brand;
+				double size;
+				double length;
+				double processsor;
+				
+				
+				void toWork(String content) {
+			
+	}
+}
+			class bottle{
+				//characteristics:- color,size,material,shape
+				// Behaviour:- carryWater()
+				
+				String color;
+				String material;
+				double size;
+				String shape;
+			
+				
+				
+				void carryWater(String content) {
+				}
+				}
+				
+				class sofa{
+					//characteristics:- color,size,material,shape,seat
+					// Behaviour:- toseat()
+					
+					String color;
+					String material;
+					double size;
+					String shape;
+					int seat;
+					
+					
+					void toseat(String content) {
+						
+					
+	}
+	
+}
+				class door{
+					//characteristics:- ,size,shape
+					// Behaviour:- tolock()
+					
+					double size;
+					String shape;
+					
+					
+					
+					void tolock(String content) {		
+	}
+}
+				class knife{
+					//characteristics:- color,size
+					// Behaviour:- tocut()
+					
+					String color;	
+					double size;
+					
+					void tocut(String content) {
+}
+	
+}
+				class dustbin{
+					//characteristics:- color,size,material,shape
+					// Behaviour:- garbage()
+					
+					String color;
+					String material;
+					double size;
+					String shape;
+			
+					void garbage(String content) {
+					}
+				}
+				
+				class shoes{
+					//characteristics:- color,size,material,brand
+					// Behaviour:- wear()
+					
+					String color;
+					String material;
+					int size;
+					String brand;
+					
+					
+					
+					void wear(String content) {
+				}
+	
+					}
+				class ring{
+					//characteristics:- color,size,material,design
+					// Behaviour :- towear()
+					
+					String color;
+					String material;
+					double size;
+					String design;
+					
+					
+					
+					void towear(String content) {
+	}
+	}
+				class Tv{
+					//characteristics:- size,shape,color
+					// Behaviour:- toWatch()
+					
+
+					double size;
+					String shape;
+					String color;
+					
+					
+					
+					void toWatch(String content) {
+}
+
+}
+	}
+}
