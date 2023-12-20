acc5e22a204d013b830a770c8c464c04b9aabfb2
SAKSHI RAJ <SAKSHI RAJ@192.168.0.12>
2023-12-20 09:57:35 +0530

    Fibonacci

---
+++ src/selenium_course1/fibonacci.java
@@ -0,0 +1,20 @@
+package selenium_course1;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		int Count=10,V1=0,V2=1,V3;
+		System.out.print(V2+" ");
+		for(int i=1;i<=Count;i++) {
+			V3=V1+V2;
+		    V1=V2;
+		    V2=V3;
+		    System.out.print(V3+" ");
+	}
+		
+		}
+		
+
+	}
+
+
