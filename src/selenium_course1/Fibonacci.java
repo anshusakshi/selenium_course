38a51e86cc2a73627887ce4133742843990f17af
SAKSHI RAJ <SAKSHI RAJ@192.168.0.3>
2024-01-27 16:52:21 +0530

    Corrected Variable name

--- src/selenium_course1/Fibonacci.java
+++ src/selenium_course1/Fibonacci.java
@@ -3,13 +3,13 @@ package selenium_course1;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		int Count=10,V1=0,V2=1,V3;
-		System.out.print(V2+" ");
+		int Count=10,v1=0,v2=1,v3;
+		System.out.print(v2+" ");
 		for(int i=1;i<=Count;i++) {
-			V3=V1+V2;
-		    V1=V2;
-		    V2=V3;
-		    System.out.print(V3+" ");
+			v3=v1+v2;
+		    v1=v2;
+		    v2=v3;
+		    System.out.print(v3+" ");
 	}
 		
 		}
