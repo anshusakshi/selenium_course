cfbfe5ee1788808215516c9501ea08870734c673
SAKSHI RAJ <SAKSHI RAJ@192.168.0.3>
2024-02-01 19:57:00 +0530

    CommentDemo

---
+++ src/selenium_course1/CommentDemo.java
@@ -0,0 +1,13 @@
+package selenium_course1;
+
+public class CommentDemo {
+	
+		/**
+		 * This method is to learn documentation comment in java.<BR\>
+		 *  This line i am writing for HTML presentation
+		 */
+	static void commentMethod(int i,int j) {
+	}
+
+
+}
