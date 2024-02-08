10c0e80c108078622f5b68f1d471a25ab4302652
SAKSHI RAJ <SAKSHI RAJ@192.168.0.8>
2024-02-08 13:24:25 +0530

    Name of variable

--- src/selenium_course1/Fibonacci.java
+++ src/selenium_course1/Fibonacci.java
@@ -3,9 +3,9 @@ package selenium_course1;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		int Count=10,v1=0,v2=1,v3;
+		int count=10,v1=0,v2=1,v3;
 		System.out.print(v2+" ");
-		for(int i=1;i<=Count;i++) {
+		for(int i=1;i<=count;i++) {
 			v3=v1+v2;
 		    v1=v2;
 		    v2=v3;
